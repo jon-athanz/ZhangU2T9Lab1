f2172829a6fd4b797958ee4f51bfa798c9845865
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-10-17 09:30:23 -0400

    ZhangU2T9Lab1

--- src/RightTriangleRunner.java
+++ src/RightTriangleRunner.java
@@ -2,6 +2,5 @@ public class RightTriangleRunner {
     public static void main(String[] args) {
         System.out.println(Math.random() * 10);
         System.out.println(Math.random());
-        System.out.println(Math.random());
     }
 }
