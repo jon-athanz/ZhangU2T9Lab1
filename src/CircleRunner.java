4c866f20666c3677765a4438bf4e796b14def14f
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-10-17 09:29:04 -0400

    ZhangU2T9Lab1

---
+++ src/CircleRunner.java
@@ -0,0 +1,10 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circ = new Circle(5.0);
+        System.out.println(circ.getInfo());
+        circ.setRadius(9.2);
+        System.out.println(circ.getInfo());
+        circ.setRadius(16.6);
+        System.out.println(circ.getInfo());
+    }
+}
---
+++ src/RightTriangleRunner.java
@@ -0,0 +1,7 @@
+public class RightTriangleRunner {
+    public static void main(String[] args) {
+        System.out.println(Math.random() * 10);
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+    }
+}
